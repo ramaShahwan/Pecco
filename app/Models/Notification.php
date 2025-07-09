bf21eb02ab6c6a8caee75a37191462830d7e2a07
ramaShahwan <119755209+ramaShahwan@users.noreply.github.com>
2025-07-09 12:36:50 +0300

    up

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Notification extends Model
 {
     use HasFactory;
+
+       
+     protected $table = 'notifications';
+
+        protected $fillable = [
+        'title',
+        'tender_id',
+        'user_id',
+        'body',
+        'is_read',
+    ];
+
+          public function User()
+      {
+          return $this->belongsTo(User::class, 'user_id');
+      }
+
+                public function Tender()
+      {
+          return $this->belongsTo(Tender::class, 'tender_id');
+      }
 }
